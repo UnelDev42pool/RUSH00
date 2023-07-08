37c7b819e5c504e547e67a84fda1203863d159e6
Killian Paris <kiparis@z4r11p1.42lyon.fr>
2023-07-08 11:18:08 +0200

    added rush00.c and rush00.h, modified rush02.h

---
+++ rush00.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush00.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kiparis <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/08 10:21:22 by kiparis           #+#    #+#             */
+/*   Updated: 2023/07/08 11:01:49 by kiparis          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_putchar.h"
+
+void	rush(int width, int height)
+{
+	int	x;
+	int	y;
+
+	y = 1;
+	while (y <= height)
+	{
+		x = 1;
+		while (x <= width)
+		{
+			if ((x == 1 || x == width) && (y == 1 || y == height))
+				ft_putchar('o');
+			else if (x == 1 || x == width)
+				ft_putchar('|');
+			else if (y == 1 || y == height)
+				ft_putchar('-');
+			else
+				ft_putchar(' ');
+			if (x == width)
+				ft_putchar('\n');
+			x++;
+		}
+		y++;
+	}
+}
